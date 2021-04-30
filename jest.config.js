fa8c84f2fc9fd17d9ef6ac34d9c59e1522eb25d1
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-04-30 18:26:20 +0200

    test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  roots: ["<rootDir>"],
+  modulePaths: ["<rootDir>"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
