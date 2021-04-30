152f1039699872cbdcd9d9b8655d7c5ca4f549a1
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-04-30 18:54:36 +0200

    revert

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions } = require("./tsconfig");
-
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  roots: ["<rootDir>"],
-  modulePaths: ["<rootDir>"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  moduleNameMapper: {
+    "@reactivedata/reactive": "<rootDir>/packages/reactive-core/src",
+  },
 };
