707f4b8051e6a5f238cac2da5e7a5136f0e99b4d
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-05-01 03:30:41 +0200

    wip

--- packages/reactive-core/src/reaction.ts
+++ packages/reactive-core/src/reaction.ts
@@ -1,11 +1,58 @@
-import { $reactive, ObserverConnection, ObserverConnectionSource } from "./observable";
+import { moduleNameMapper } from "jest.config";
+import { $reactive, InternalObservable, ObserverConnection, ObserverConnectionSource } from "./observable";
 import { Observer } from "./observer";
 
 let runningReactions: Reaction[] = [];
 
 export class Reaction implements Observer {
-  public observing = new Set<ObserverConnection<object>>();
+  public observingToDispose = new Map<
+    InternalObservable<object>,
+    {
+      iterate: false | true;
+      byKey: Set<string | number>;
+    }
+  >();
+
+  public observing = new Map<
+    InternalObservable<object>,
+    {
+      iterate: false | true;
+      byKey: Set<string | number>;
+    }
+  >();
+
   constructor(private func: () => void | Promise<void>, private options: { name: string }) {}
+
+  public registerConnection<T extends object>(source: ObserverConnectionSource<T>) {
+    let existing = this.observing.get(source.observable);
+    if (!existing) {
+      existing = {
+        byKey: new Set(),
+        iterate: false,
+      };
+      this.observing.set(source.observable, existing);
+    }
+    if (source.type === "iterate") {
+      if (this.observingToDispose.get(source.observable)?.iterate) {
+        this.observingToDispose.get(source.observable).iterate = false;
+      }
+      const wasRegistered = existing.iterate;
+      existing.iterate = true;
+      return !!wasRegistered;
+    } else {
+      if (this.observingToDispose.get(source.observable)?.byKey.has(source.key)) {
+        this.observingToDispose.get(source.observable).byKey.delete(source.key);
+      }
+
+      let existingKey = existing.byKey.has(source.key);
+      if (!existingKey) {
+        existing.byKey.add(source.key);
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 
   private reaction = () => {
     runningReactions.push(this);
@@ -22,16 +69,19 @@ export class Reaction implements Observer {
     if (runningReactions.includes(this)) {
       throw new Error("already running reaction");
     }
-    this.observing.forEach((val) => {
-      const connections = val.source.observable[$reactive].connections;
-      if (val.source.type === "iterate") {
-        connections.iterate.delete(val);
-      } else {
-        connections.byKey.get(val.source.key).delete(val);
-      }
-    });
-    this.observing.clear();
+    this.observingToDispose = this.observing;
+    this.observing = new Map();
     this.reaction();
+
+    this.observingToDispose.forEach((val, key) => {
+      if (val.iterate) {
+        key[$reactive].connections.iterate.delete(this);
+      }
+      val.byKey.forEach((subkey) => {
+        key[$reactive].connections.byKey.delete(subkey);
+      });
+    });
+    this.observingToDispose = new Map();
   }
 }
 
