72177574e96343607fbbcb477dca35cda50be0d9
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-05-03 11:11:25 +0200

    add atom

---
+++ packages/reactive-core/src/atom.ts
@@ -0,0 +1,19 @@
+import { reactive } from "./observable";
+
+// Hacky, it's not really an Atom in the sense that it is not the building block of observables
+export class Atom {
+  private _observable = reactive({ _key: 1 });
+
+  public reportObserved() {
+    return (this._observable._key as any) as void;
+  }
+
+  public reportChanged() {
+    this._observable._key++;
+  }
+}
+
+export function createAtom(name: string, onBecomeObservedHandler?: () => void, onBecomeUnobservedHandler?: () => void) {
+  // TODO: add support for params
+  return new Atom();
+}
